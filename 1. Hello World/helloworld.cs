1d6c1ecef7de89991e7f50f2efeac8358bdb3ef2
BayernMuller <cjjunk29@gmail.com>
2020-12-29 22:44:15 +0900

    [Add] Hello World

---
+++ 1. Hello World/helloworld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace LearningCsharp // namespace
+{
+	class Program // class name
+	{
+		static void Main(string[] args) // Main function (Entry Point)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
